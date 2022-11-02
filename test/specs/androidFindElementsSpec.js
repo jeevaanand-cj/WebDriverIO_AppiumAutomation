41968b21f634b7f6acad675afc5d818788e66e49
Nethaji, Dinesh [ETHUS NON-J&J] <DNethaji@its.jnj.com>
2022-11-02 22:39:07 +0530

    Basic code in appium

---
+++ test/specs/androidFindElementsSpec.js
@@ -0,0 +1,6 @@
+describe('Android Find Elements',() =>{
+
+    it('Find element by accessibility id', async() =>{
+
+    });// it - block is used to iterate the tests one by one
+});// describe - block is used to collect all the test in one block
